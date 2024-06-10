414bb2295d972b2e54d1f6546092cfb1202e6b79
AlexaZ06 <122847494+AlexaZ06@users.noreply.github.com>
2024-06-10 13:47:08 -0600

    started website

---
+++ satire-website/sketch.js
@@ -0,0 +1,34 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function mainpage() {
+
+}
+
+function artice(){
+
+}
+
+function comic(){
+
+}
+
+function song(){
+
+}
